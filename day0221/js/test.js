e1e3a443d9be9e16dbcfe6d489cc191923ab8022
yeojinyun <yeojinable@naveer.com>
2023-02-21 16:07:54 +0900

    2-21-화요일 test2

---
+++ day0221/js/test.js
@@ -0,0 +1,16 @@
+var user_name = "블랙자몽" ; 
+var user_num = 379 ;
+document.write('javascript이름=', user_name, '<br>') ;
+document.write('javascript숫자=', user_num) ;
+document.write('<hr size=5 color=blue>');
+
+//국어kor=90 영어eng=85  총점total=175 평균avg=87.5
+//문제1 변수에 값대입, 연산 - 더하기,몫
+var kor = 90 ;
+var eng = 85 ;
+var total = kor + eng ; 
+var avg = total/2 ; 
+document.write('국어점수 = ' , kor , '<br>') ;
+document.write('영어점수 = ' , eng , '<br>') ;
+document.write('총점점수 = ' , total, '<br>') ;
+document.write('평균점수 = ' , avg, '<br>') ;
